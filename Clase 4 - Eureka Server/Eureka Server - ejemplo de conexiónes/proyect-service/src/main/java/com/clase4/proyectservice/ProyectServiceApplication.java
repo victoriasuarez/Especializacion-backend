7c2e1de003670e3cc17bc0b18c1b735ca2340de4
victoriasuarez <victoriasuarez49@gmail.com>
2022-10-26 21:25:28 -0300

    Agregando ejemplo de conexiones de servidores

---
+++ Clase 4 - Eureka Server/Eureka Server - ejemplo de conexiónes/proyect-service/src/main/java/com/clase4/proyectservice/ProyectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clase4.proyectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectServiceApplication.class, args);
+	}
+
+}
---
+++ Clase 4 - Eureka Server/Eureka Server - ejemplo de conexiónes/user-service/src/main/java/com/Clase4/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Clase4.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
